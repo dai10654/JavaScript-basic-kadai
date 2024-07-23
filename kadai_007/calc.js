ea40ef39d6c3684fce7a1d32c5bb499d1ee04a12
dai10654 <dafuji120@outlook.jp>
2024-07-23 17:48:14 +0900

    kadai_007を追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+// 1から100までのランダムな整数を生成
+const num = Math.floor(Math.random() * 100) + 1;
+console.log(num);
+
+
+if (num % 3 === 0 && num % 5 === 0) {
+  // ③ 3と5の倍数の場合
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  // ① 3の倍数の場合
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
+  // ② 5の倍数の場合
+  console.log("5の倍数です");
+} else {
+  // どれにも当てはまらない場合
+  console.log("3の倍数でも5の倍数でもありません");
+}
